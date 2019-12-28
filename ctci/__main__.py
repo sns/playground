5276edc3ff342865675127eeed6524d8df9593a1
Sina Salehi <sinasalehi@L321-SinaSalehi.local>
2019-12-28 13:23:42 -0500

    modified comment

--- ctci/__main__.py
+++ ctci/__main__.py
@@ -1,5 +1,5 @@
-#import the main method for a problem module
+#import the solve method for a problem module
 from ctci.problems.chapter1.string_has_all_unique_characters import solve
 if __name__ == '__main__':
-    #run the main method imported above
+    #run the solve method imported above
     solve()
